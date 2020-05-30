38dc16467c23100906f0ec14c82877949a837e9b
AceIsHuman <menoman1995@yahoo.com>
2020-05-30 12:05:12 -0500

    define append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,7 +6,16 @@ class RingBuffer:
         self.oldest_index = 0
 
     def append(self, item):
-        pass
+        if self.size == self.capacity:
+            self.storage[self.oldest_index] = item
+            capacity = self.capacity - 1
+            if self.oldest_index < capacity:
+                self.oldest_index += 1 
+            else: self.oldest_index = 0
+
+        else:
+            self.storage.append(item)
+            self.size += 1
 
     def get(self):
         pass
